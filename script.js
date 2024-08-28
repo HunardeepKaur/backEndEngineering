5351726427385927ba7a99ce92bcc967413b4100
hunardeep kaur <hunar192005@gmail.com>
2024-08-28 13:40:13 +0530

    28-August,mongodb,mongosh,crud

---
+++ script.js
@@ -0,0 +1,23 @@
+const data=[
+    [
+        {"name":"himanshu","age":19,"marks":99,"subject":"backend"},
+        {"name":"hunar","age":18,"marks":100,"subject":"frontend"},
+        {"name":"harmeet","age":28,"marks":88,"subject":"BEE"},
+        {"name":"abcd","age":38,"marks":77,"subject":"FEE"},
+        {"name":"efgh","age":48,"marks":66,"subject":"hindi"},
+    ],
+    [
+        {"name":"himanshu","age":19,"marks":99,"subject":"backend"},
+        {"name":"hunar","age":18,"marks":100,"subject":"frontend"},
+        {"name":"harmeet","age":28,"marks":88,"subject":"BEE"},
+        {"name":"abcd","age":38,"marks":77,"subject":"FEE"},
+        {"name":"efgh","age":48,"marks":66,"subject":"hindi"},
+    ],
+    [
+        {"name":"abcd","age":56,"subject":"FEE","rating":5},
+        {"name":"efgh","age":46,"subject":"BEE","rating":4},
+        {"name":"ijkl","age":36,"subject":"backend","rating":3},
+        {"name":"zxcv","age":26,"subject":"DBMS","rating":2},
+        {"name":"bnmv","age":66,"subject":"CN","rating":1},
+    ]
+]
